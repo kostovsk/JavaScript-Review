d8d43d918cd3b543c5b268a74a5d15f7aeb292a5
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-03 23:19:08 -0600

    ternary operator

---
+++ ternary_operator.js
@@ -0,0 +1,10 @@
+const age = 21;
+let message = (age >= 18) ? "You shall pass" : "You shall not pass";
+
+// if (age >= 18) {
+//     message = 'You shall pass';
+// } else {
+//     message = 'You shall not pass';
+// }
+
+console.log(message);
