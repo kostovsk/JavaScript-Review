68d953957446e52c66edb195ad7fe54442d5aade
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-07 22:53:28 -0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,40 @@
+let person = {
+    firstName: "Stoycho",
+    lastName: "Kostov",
+    age: 32,
+    currentDate: new Date(),
+};
+
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+
+const {firstName, lastName, age} = person;
+
+// console.log(firstName);
+
+getAddress = () => {
+    return {
+        street: 'street 1',
+        country: 'country 1',
+        state: 'state 1'
+    }
+}
+
+const {street, country} = getAddress();
+
+// printName = (person) => {
+//     console.log(person.firstName);
+// }
+
+printName2 = ({firstName}) => {
+    console.log(firstName);
+}
+
+// printName2(person);
+
+const arr = [1,2,3,4];
+
+const [first, second,,fourth] = arr;
+
+console.log(second);
