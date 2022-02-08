64cb993c2c5418e55109611b409f67b0d12adb7b
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-07 23:21:29 -0600

    spread operator - object

--- spread_operator.js
+++ spread_operator.js
@@ -20,9 +20,38 @@ const concatenatedArrays = [...numbers, ...otherNumbers]; // [2, 3, 4, 5]
 // example 4
 
 const [first, ...theRemainingValues] = concatenatedArrays;
-console.log(first); // 2
-console.log(theRemainingValues); // [3,4,5]
+// console.log(first); // 2
+// console.log(theRemainingValues); // [3,4,5]
 
 // example 5
 
 const concatenatedArraysCloned = [...concatenatedArrays];
+
+// example 6
+
+const person = {
+    name: 'Stoycho',
+    lastName: 'Kostov'
+};
+
+const person2 = {
+    ...person,
+    age: 32
+}
+
+// console.log(person2);
+
+// example 7
+
+const person3 = {...person2};
+person3.name = 'Scott';
+
+console.log(person2);
+console.log(person3);
+
+// example 8
+
+const {age, ...person4} = person3;
+
+console.log(age);
+console.log(person4);
